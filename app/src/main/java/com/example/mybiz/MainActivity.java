d011023228eacc451009ffca2c90296eb192b666
Udari Kaushalya <ukliyange218@gmail.com>
2019-09-07 13:09:53 +0530

    1st push

---
+++ app/src/main/java/com/example/mybiz/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.mybiz;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn_add;
+//    ActionBar actionBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        pressAdd();
+
+//        actionBar = getSupportActionBar();
+//        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#000080")));
+    }
+
+    public void pressAdd(){
+        btn_add =(Button)findViewById(R.id.add_btn);
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,AddCreditor.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
