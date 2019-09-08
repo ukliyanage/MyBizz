9949a44b3392ff2119c37aa3f815e1cf5a358010
Udari Kaushalya <ukliyange218@gmail.com>
2019-09-08 13:27:56 +0530

    gosh

--- app/src/main/java/com/example/mybiz/DebitorMainActivity.java
+++ app/src/main/java/com/example/mybiz/DebitorMainActivity.java
@@ -6,18 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class DebitorHome extends AppCompatActivity {
+public class DebitorMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_debitor_home);
+        setContentView(R.layout.activity_debitor_main);
     }
 
-
     public void pressAdd(View view){
-        Intent intent = new Intent(this,AddDebitor.class);
+        Intent intent = new Intent(this,AddCreditor.class);
         startActivity(intent);
 
     }
+
 }
